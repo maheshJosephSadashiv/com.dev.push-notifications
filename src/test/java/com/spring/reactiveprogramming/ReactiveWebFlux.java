5c42ce99e237b000ab95e8b46eb2a2336942d50e
maheshJosephSadashiv <mahesh.sadashiv@gmail.com>
2023-02-06 00:03:29 +0530

    On error

--- src/test/java/com/spring/reactiveprogramming/ReactiveWebFlux.java
+++ src/test/java/com/spring/reactiveprogramming/ReactiveWebFlux.java
@@ -6,9 +6,19 @@ import reactor.core.publisher.Mono;
 public class ReactiveWebFlux {
 
     @Test
-    public void monoTest(){
+    public void monoSuccessTest(){
         Mono<String> monoString = Mono.just("This is a test!").log();
         monoString.subscribe(System.out::println);
+    }
+
+    @Test
+    public void monoWithErrorTest(){
+        Mono<?> monoString = Mono.just("This is a test!")
+                .then(Mono.error(new RuntimeException("An Exception has occurred")))
+                .log();
+        monoString.subscribe(System.out::println, throwable -> {
+            System.out.println(throwable.getMessage());
+        });
     }
 
 }
